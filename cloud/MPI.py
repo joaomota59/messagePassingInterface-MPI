f28b293d5738501231baeb583ab05618b6f10435
michael <mlmbufpi@gmail.com>
2021-05-20 18:32:54 -0300

    Add a funcao feita pelo Joao de novo

--- cloud/MPI.py
+++ cloud/MPI.py
@@ -1,6 +1,15 @@
 from mpi4py import MPI
+
+
+def mpiPI(i=1,N=840):#funcao que calcula o valor aprox de pi
+    somatorio = 0
+    for j in range(i,N+1):
+        somatorio += 1/(1+((j-0.5)/N)**2)
+    return (somatorio/N)*4
+
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
-proc = MPI.Get_processor_name()
-print("Ola processo",rank,"na maquina ",proc)
+idmaquina = MPI.Get_processor_name()
+print("Ola processo",rank,"na maquina ",idmaquina)
+print("oi")
 
