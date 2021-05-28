fbdc03c2689ee3cda3f3355e209f527dceb159f5
João Lucas Mota <54906718+joaomota59@users.noreply.github.com>
2021-05-28 18:16:40 -0300

    acrescentando o tempo de execução

--- cloud/MPI.py
+++ cloud/MPI.py
@@ -36,10 +36,23 @@ if rank == 1:
 if __name__ == "__main__": #main -- Segunda versão
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()#rank do processo atual
+    numDeProcessos = comm.Get_size()#numero de processos
     idmaquina = MPI.Get_processor_name()#hostname damaquina
+    comm.Barrier()
+    tinicial = MPI.Wtime()#tempo inicial
     res1 = mpiPI()
+    comm.Barrier()
+    tfinal=MPI.Wtime()#tempo final
     k = ("Resposta do processo [" + str(rank) + "] = " + str(res1) + " ID Máquina = "+str(idmaquina))
     print("-"*len(k)+"\n"+k+"\n")
+    if rank == 0:
+        bufferAux = [tfinal-tinicial]
+        for i in range(1,numDeProcessos):
+            bufferAux.append(comm.recv(source = i))
+        print("Tempo de execução:",max(bufferAux))#tempo do processo que durou mais
+    else:
+        comm.send(tfinal-tinicial,dest = 0)
+
     #Elementos na comunicação
     #print("Quantidade de processos no comunicador = " + str(comm.Get_size()))
     
--- cloud/MPI2.py
+++ cloud/MPI2.py
@@ -42,7 +42,19 @@ if rank == 1:
 if __name__ == "__main__": #main -- Segunda versão
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()#rank do processo atual
+    numDeProcessos = comm.Get_size()#numero de processos
     idmaquina = MPI.Get_processor_name()#hostname damaquina
+    comm.Barrier()
+    tinicial = MPI.Wtime()
     res1 = mpiPI(comm.Get_size(),rank)
+    comm.Barrier()
+    tfinal=MPI.Wtime()
     k = ("Resposta do processo [" + str(rank) + "] = " + str(res1) + " ID Máquina = "+str(idmaquina))
     print("-"*len(k)+"\n"+k+"\n")
+    if rank == 0:
+        bufferAux = [tfinal-tinicial]
+        for i in range(1,numDeProcessos):
+            bufferAux.append(comm.recv(source = i))
+        print("Tempo de execução:",max(bufferAux))#tempo do processo que durou mais
+    else:
+        comm.send(tfinal-tinicial,dest = 0)
