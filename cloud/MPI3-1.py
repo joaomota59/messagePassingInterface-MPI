46560fe31b4a0e61331a6dd8ca18ce160bee36d3
João Lucas Mota <54906718+joaomota59@users.noreply.github.com>
2021-05-28 21:58:02 -0300

    ajustes na barreira

---
+++ cloud/MPI3-1.py
@@ -0,0 +1,56 @@
+from mpi4py import MPI
+
+def mpiPI(nroProcesso, rank):#funcao que calcula o valor aprox de pi
+    N = 840
+    i = int(1 + (N/nroProcesso)*rank)
+    k = int((N/nroProcesso)*(rank+1))
+    somatorio = 0
+    for j in range(i,k+1):
+        somatorio += 1/(1+((j-0.5)/N)**2)
+    #print(i,k)#intervalos
+    #print((somatorio/N)*4)#somatorio de cada intervalo
+    return (somatorio/N)*4
+
+#1 fazer com que todos calculem o valor de PI 
+#PS: Falta medir o tempo!!
+#print("ID do processo = ["+str(rank)+"] na maquina "+str(idmaquina) + " PI = " + str(mpiPI()))
+
+if __name__ == "__main__": #main -- Terceira versão
+    comm = MPI.COMM_WORLD
+    numDeProcessos = comm.Get_size()
+    rank = comm.Get_rank()#rank do processo atual
+    buffer = []
+    if(840 % numDeProcessos != 0):#retorna erro se o numero de processos nao for divisivel p 840
+        if rank == 0 :
+            print("ERRO!\nEntre com um número de processos que seja divisivel por 840!")
+    else:#se for divisivel por 840 entao divide entre os processos
+        idmaquina = MPI.Get_processor_name()#hostname damaquina
+        if rank == 0:
+            tinicial = MPI.Wtime()#tempo inicial
+            res1 = mpiPI(numDeProcessos,rank)
+            tfinal=MPI.Wtime()#tempo final
+            #k = ("Resposta do processo [" + str(rank) + "] = " + str(res1) + " ID Máquina = "+str(idmaquina))
+            #print("-"*len(k)+"\n"+k+"\n")
+            bufferAux = [tinicial - tfinal]
+            for i in range(1,numDeProcessos):
+                bufferAux.append(comm.recv(source = i)[1])
+            print("Tempo de execução:",max(bufferAux))#tempo de execução do processo que demorou mais
+
+            '''
+            buffer = [res1]#buffer que guarda todos os resultados dos processos
+            for i in range(1,numDeProcessos):
+                buffer.append(comm.recv(source = i)[0])
+            print("Soma de todos processos:",sum(buffer))
+            '''
+            
+        else:#se for qualquer processo diferente do processo 1
+            
+            tinicial = MPI.Wtime()#tempo inicial
+            
+            res1 = mpiPI(numDeProcessos,rank)
+
+            tfinal=MPI.Wtime()#tempo final
+            comm.send([res1,tfinal-tinicial],dest = 0)
+            #print("Tempo: ",tfinal-tinicial)
+            #k = ("Resposta do processo [" + str(rank) + "] = " + str(res1) + " ID Máquina = "+str(idmaquina))
+            #print("-"*len(k)+"\n"+k+"\n")
---
+++ cloud/MPI3-2.py
@@ -0,0 +1,49 @@
+from mpi4py import MPI
+import numpy as np
+
+def mpiPI(nroProcesso, rank):#funcao que calcula o valor aprox de pi
+    N = 840
+    i = int(1 + (N/nroProcesso)*rank)
+    k = int((N/nroProcesso)*(rank+1))
+    somatorio = 0
+    for j in range(i,k+1):
+        somatorio += 1/(1+((j-0.5)/N)**2)
+    #print(i,k)#intervalos
+    #print((somatorio/N)*4)#somatorio de cada intervalo
+    return (somatorio/N)*4
+
+#1 fazer com que todos calculem o valor de PI 
+#PS: Falta medir o tempo!!
+#print("ID do processo = ["+str(rank)+"] na maquina "+str(idmaquina) + " PI = " + str(mpiPI()))
+
+if __name__ == "__main__": #main -- Terceira versão
+    comm = MPI.COMM_WORLD
+    numDeProcessos = comm.Get_size()
+    rank = comm.Get_rank()#rank do processo atual
+    buffer = []
+    if(840 % numDeProcessos != 0):#retorna erro se o numero de processos nao for divisivel p 840
+        if rank == 0 :
+            print("ERRO!\nEntre com um número de processos que seja divisivel por 840!")
+    else:#se for divisivel por 840 entao divide entre os processos
+        idmaquina = MPI.Get_processor_name()#hostname damaquina
+        if rank == 0:
+            res1 = mpiPI(numDeProcessos,rank)   
+            buffer = [[res1]]#buffer que guarda todos os resultados dos processos
+            for i in range(1,numDeProcessos):
+                buffer.append(comm.recv(source = i))
+            tfinal=MPI.Wtime()#tempo final é igual para todos processos
+            tinicialDefinitivo = np.array(buffer[1:]).min(axis=0)[1]#pega o menor tempo inicial dos processos
+            print("Tempo de execução:",tfinal - tinicialDefinitivo)#tempo de execução em relação ao processo que demorou mais
+            totalPI = np.array(buffer[1:]).sum(axis=0)[0]+res1
+            print("Soma de todos processos:",totalPI)#exibe a soma de todos processos
+            
+            
+        else:#se for qualquer processo diferente do processo 1
+            res1 = mpiPI(numDeProcessos,rank)
+            tinicial = MPI.Wtime()#tempo inicial
+            comm.send([res1,tinicial],dest = 0)
+            #print("Inicio",tinicial,"rank:",rank)
+            #k = ("Resposta do processo [" + str(rank) + "] = " + str(res1) + " ID Máquina = "+str(idmaquina))
+            #print("-"*len(k)+"\n"+k+"\n")      
+            #k = ("Resposta do processo [" + str(rank) + "] = " + str(res1) + " ID Máquina = "+str(idmaquina))
+            #print("-"*len(k)+"\n"+k+"\n")
